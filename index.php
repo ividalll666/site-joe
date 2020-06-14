5ac9e1e48836964cfad66105659392c3d4cc2063
Itallo Vidal <48143354+ividalll666@users.noreply.github.com>
2020-06-13 22:25:28 -0300

    Site com o joe

---
+++ index.php
@@ -0,0 +1,156 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="css/styles.css">
+
+        <title>Belo Site</title>
+
+    </head>
+    <body>
+
+        <!--  Cabecalho da página: onde ficará a nav e o nome dela -->
+         <header>
+
+            <!-- container que engloba a nav -->
+            <nav id="container-navbar">
+
+                <!-- nossa lista trasnformada em nav -->
+                <ul id="navbar">
+                    <!-- cada list item é um navbar-item agora -->
+                    <li class="navbar-item"> <a href="">    Ínicio      </a>        </li>
+                    <li class="navbar-item"> <a href="">    Notícias </a>        </li>
+                    <li class="navbar-item"> <a href="">    Contato</a>        </li>
+                    <li class="navbar-item"> <a href="">    Sobre     </a>        </li>
+                </ul>
+            </nav>
+
+            <!-- nome da página -->
+        <h1 >Grande Página Conteudista</h1>
+        <p>Grande subtítulo da Página Conteudista</p>
+
+        </header>
+
+        <!-- tag que engloba nosso conteúdo principal -->
+        <main id="container-geral">
+                
+            <!-- tag que englobará nossa matéria -->
+            <section class="container-conteudo">
+
+                <!-- titulo da matéria -->
+                 <h1>A porra da diferenca entre homem e mulher</h1>
+
+                 <!-- ponto do homem -->
+                <article  class="materia homem">
+                    
+                        <h2> Texto do Homem</h2>
+
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum libero voluptatum, dignissimos ducimus reiciendis maxime velit 
+                            nemo consequuntur deserunt inventore obcaecati autem harum voluptatem dolores reprehenderit cumque eaque eveniet quis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum libero voluptatum, dignissimos ducimus reiciendis maxime velit 
+                            nemo consequuntur deserunt inventore obcaecati autem harum voluptatem dolores reprehenderit cumque eaque eveniet quis!  Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum libero voluptatum, dignissimos ducimus reiciendis maxime velit 
+                            nemo consequuntur deserunt inventore obcaecati autem harum voluptatem dolores reprehenderit cumque eaque eveniet quis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum libero voluptatum, dignissimos ducimus reiciendis maxime velit 
+                            nemo consequuntur deserunt inventore obcaecati autem harum vo  luptatem dolores reprehenderit cumque eaque eveniet quis!
+                        </p>
+
+
+                    <figure class="container-figure">
+                        <img  src="imagens/1.jpg" alt="Robertinho">
+                    </figure>
+
+                </article>
+
+                <!-- ponto da mulher -->
+                <article class ="materia mulher">
+
+                        <h2>Texto da Mulher</h2>
+                        
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum libero voluptatum, dignissimos ducimus reiciendis maxime velit 
+                            nemo consequuntur deserunt inventore obcaecati autem harum voluptatem dolores reprehenderit cumque eaque eveniet quis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum libero voluptatum, dignissimos ducimus reiciendis maxime velit 
+                            nemo consequuntur deserunt inventore obcaecati autem harum voluptatem dolores reprehenderit cumque eaque eveniet quis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum libero voluptatum, dignissimos ducimus reiciendis maxime velit 
+                            nemo consequuntur deserunt inventore obcaecati autem harum voluptatem dolores reprehenderit cumque eaque eveniet quis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum libero voluptatum, dignissimos ducimus reiciendis maxime velit 
+                            nemo consequuntur deserunt inventore obcaecati autem harum voluptatem dolores reprehenderit cumque eaque eveniet quis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum libero voluptatum, dignissimos ducimus reiciendis maxime velit 
+                            nemo consequuntur deserunt inventore obcaecati autem harum voluptatem dolores reprehenderit cumque eaque eveniet quis!
+                        </p>
+
+                    <figure class="container-figure">
+                        <img  src="imagens/2.jpg" alt="Elizabeth Evans">
+                    </figure>
+
+                </article>
+
+            </section>
+
+            <section class="container-conteudo conteudo2">
+
+                <!-- titulo da matéria -->
+                 <h1> E como eles são dois fodidos</h1>
+
+                <article  class="materia ser-humano">
+                    
+                        <h2> Texto do Ser humano</h2>
+                        
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum libero voluptatum, dignissimos ducimus reiciendis maxime velit 
+                            nemo consequuntur deserunt inventore obcaecati autem harum voluptatem dolores reprehenderit cumque eaque eveniet quis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum libero voluptatum, dignissimos ducimus reiciendis maxime velit 
+                            nemo consequuntur deserunt inventore obcaecati autem harum voluptatem dolores reprehenderit cumque eaque eveniet quis!  Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum libero voluptatum, dignissimos ducimus reiciendis maxime velit 
+                            nemo consequuntur deserunt inventore obcaecati autem harum voluptatem dolores reprehenderit cumque eaque eveniet quis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum libero voluptatum, dignissimos ducimus reiciendis maxime velit 
+                            nemo consequuntur deserunt inventore obcaecati autem harum vo  luptatem dolores reprehenderit cumque eaque eveniet quis!
+                        </p>
+
+
+                    <figure class="container-figure">
+                        <img  src="imagens/3.jpg" alt="Robertinho">
+                    </figure>
+
+                </article>
+
+            </section>
+
+
+            <section class="container-conteudo conteudo3">
+                    
+                <h1> Top Seres Humanos Aleatórios</h1>
+
+                <article class="galeria-grid">
+
+                    <figure id="foto1">
+                        <img src="imagens/1.jpg" alt="">
+                    </figure>
+
+                    <figure id="foto2">
+                        <img src="imagens/2.jpg" alt="">
+                    </figure>
+
+                    <figure id="foto3">
+                        <img  id="foto3-pos" src="imagens/3.jpg" alt="">
+                    </figure>
+
+                    <figure id="foto4">
+                        <img src="imagens/4.jpg" alt="">
+                    </figure>
+
+                    <figure id="foto5">
+                        <img src="imagens/5.jpeg" alt="">
+                    </figure>
+
+                    <figure id="foto6">
+                        <img src="imagens/6.jpg" alt="">
+                    </figure>
+
+                    <figure id="foto7">
+                        <img src="imagens/7.jpg" alt="">
+                    </figure>
+
+                </article>
+
+            </section>
+
+        </main>
+            
+        <!-- footer: rodapé -->
+        <footer>
+
+            <h4>Direitos reservados. Copyright © 2020</h4>
+            <p> Site criado para estudos por Vidal e Joezinho </p>
+        </footer>
+
+
+    </body>
+</html>
